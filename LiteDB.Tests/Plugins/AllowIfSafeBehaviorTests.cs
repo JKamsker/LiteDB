3ef49168f325da03e05cde117edaaa99bae4a4cd
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-03-02 03:39:32 +0100

    Cover AllowIfSafe DDL refusal

--- LiteDB.Tests/Plugins/AllowIfSafeBehaviorTests.cs
+++ LiteDB.Tests/Plugins/AllowIfSafeBehaviorTests.cs
@@ -18,7 +18,7 @@ namespace LiteDB.Tests.Plugins
             using (var pluginDatabase = new LiteDatabase(file.Filename, plugins: new[] { VectorSearchPlugin.Instance }))
             {
                 var collection = pluginDatabase.GetCollection<TestDocument>("docs");
-                collection.Insert(new TestDocument { Id = 1, Embedding = new[] { 0.1f, 0.2f, 0.3f } });
+                collection.Insert(new TestDocument { Id = 1, Name = "alpha", Embedding = new[] { 0.1f, 0.2f, 0.3f } });
                 collection.EnsureIndex(x => x.Embedding, new VectorIndexOptions(3)).Should().BeTrue();
             }
 
@@ -32,7 +32,7 @@ namespace LiteDB.Tests.Plugins
             var affected = vanillaDatabase.GetCollection<TestDocument>("docs");
             affected.Count().Should().Be(1);
 
-            Action write = () => affected.Insert(new TestDocument { Id = 2, Embedding = new[] { 0.5f, 0.6f, 0.7f } });
+            Action write = () => affected.Insert(new TestDocument { Id = 2, Name = "beta", Embedding = new[] { 0.5f, 0.6f, 0.7f } });
             write.Should().Throw<LiteException>()
                 .Where(ex => ex.ErrorCode == LiteException.PLUGIN_REQUIRED);
 
@@ -41,9 +41,80 @@ namespace LiteDB.Tests.Plugins
             clean.Count().Should().Be(1);
         }
 
+        [Fact]
+        public void AllowIfSafe_should_refuse_ddl_on_affected_collections_without_partial_mutation()
+        {
+            using var file = new TempFile();
+
+            using (var pluginDatabase = new LiteDatabase(file.Filename, plugins: new[] { VectorSearchPlugin.Instance }))
+            {
+                var collection = pluginDatabase.GetCollection<TestDocument>("docs");
+                collection.Insert(new TestDocument { Id = 1, Name = "alpha", Embedding = new[] { 0.1f, 0.2f, 0.3f } });
+                collection.EnsureIndex("embedding_idx", x => x.Embedding, new VectorIndexOptions(3)).Should().BeTrue();
+            }
+
+            using (var vanillaDatabase = new LiteDatabase(file.Filename, new LiteDatabaseOptions
+            {
+                MissingPluginBehavior = PluginMissingBehavior.AllowIfSafe
+            }))
+            {
+                Action act = () => vanillaDatabase.DropCollection("docs");
+
+                act.Should().Throw<LiteException>()
+                    .Where(ex => ex.ErrorCode == LiteException.PLUGIN_REQUIRED);
+
+                var docs = vanillaDatabase.GetCollection<TestDocument>("docs");
+                docs.Count().Should().Be(1);
+
+                Action ensureIndex = () => docs.EnsureIndex(x => x.Name);
+
+                ensureIndex.Should().Throw<LiteException>()
+                    .Where(ex => ex.ErrorCode == LiteException.PLUGIN_REQUIRED);
+            }
+
+            using (var reopened = new LiteDatabase(file.Filename, plugins: new[] { VectorSearchPlugin.Instance }))
+            {
+                var docs = reopened.GetCollection<TestDocument>("docs");
+                docs.Count().Should().Be(1);
+                docs.EnsureIndex("embedding_idx", x => x.Embedding, new VectorIndexOptions(3)).Should().BeFalse();
+            }
+        }
+
+        [Fact]
+        public void AllowIfSafe_should_not_plan_against_plugin_indexes_when_plugin_is_missing()
+        {
+            using var file = new TempFile();
+
+            using (var pluginDatabase = new LiteDatabase(file.Filename, plugins: new[] { VectorSearchPlugin.Instance }))
+            {
+                var collection = pluginDatabase.GetCollection<TestDocument>("docs");
+                collection.Insert(new[]
+                {
+                    new TestDocument { Id = 1, Name = "alpha", Embedding = new[] { 0.1f, 0.2f, 0.3f } },
+                    new TestDocument { Id = 2, Name = "beta", Embedding = new[] { 0.3f, 0.2f, 0.1f } }
+                });
+
+                collection.EnsureIndex("embedding_idx", x => x.Embedding, new VectorIndexOptions(3)).Should().BeTrue();
+            }
+
+            using var vanillaDatabase = new LiteDatabase(file.Filename, new LiteDatabaseOptions
+            {
+                MissingPluginBehavior = PluginMissingBehavior.AllowIfSafe
+            });
+
+            var plan = vanillaDatabase.GetCollection<TestDocument>("docs")
+                .Query()
+                .OrderBy(x => x.Embedding)
+                .GetPlan();
+
+            plan["index"]["name"].AsString.Should().Be("_id");
+        }
+
         private sealed class TestDocument
         {
             public int Id { get; set; }
+
+            public string Name { get; set; }
 
             public float[] Embedding { get; set; }
         }
@@ -56,4 +127,3 @@ namespace LiteDB.Tests.Plugins
         }
     }
 }
-
